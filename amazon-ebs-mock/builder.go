2eb77c88aba578ac55720f9bc6ab625410985c76
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-08-04 20:04:36 +0200

    Add amazon-ebs-mock/builder.go, the first real implementation

--- amazon-ebs-mock/builder.go
+++ amazon-ebs-mock/builder.go
@@ -2,31 +2,31 @@ package amazonebsmock
 
 import (
 	"log"
-        "math/rand"
-        "time"
+	"math/rand"
+	"time"
 
+	awscommon "github.com/hashicorp/packer/builder/amazon/common"
 	"github.com/hashicorp/packer/common"
 	"github.com/hashicorp/packer/packer"
-	awscommon "github.com/hashicorp/packer/builder/amazon/common"
 )
 
 const BuilderId = "horgix.amazonebsmock"
 
-var regions = [...]string {
-  "us-east-2",
-  "us-east-1",
-  "us-west-1",
-  "us-west-2",
-  "ca-central-1",
-  "ap-south-1",
-  "ap-northeast-2",
-  "ap-southeast-1",
-  "ap-southeast-2",
-  "ap-northeast-1",
-  "eu-central-1",
-  "eu-west-1",
-  "eu-west-2",
-  "sa-east-1",
+var regions = [...]string{
+	"us-east-2",
+	"us-east-1",
+	"us-west-1",
+	"us-west-2",
+	"ca-central-1",
+	"ap-south-1",
+	"ap-northeast-2",
+	"ap-southeast-1",
+	"ap-southeast-2",
+	"ap-northeast-1",
+	"eu-central-1",
+	"eu-west-1",
+	"eu-west-2",
+	"sa-east-1",
 }
 
 type Config struct {
@@ -39,8 +39,8 @@ type Builder struct {
 
 func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 	log.Println("Hello I'm a custom builder and this is the Prepare step")
-        log.Println("Initializing random generator...")
-        rand.Seed(time.Now().Unix())
+	log.Println("Initializing random generator...")
+	rand.Seed(time.Now().Unix())
 	return nil, nil
 }
 
@@ -54,13 +54,14 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 
 	amis := make(map[string]string)
 
-        for i := 0; i<10; i++ {
-          amis[regions[rand.Intn(len(regions))]] = "ami-12345678"
-        }
+	for i := 0; i < 10; i++ {
+		amis[regions[rand.Intn(len(regions))]] = "ami-12345678"
+	}
 
 	artifact := &awscommon.Artifact{
 		Amis: amis,
 	}
+	ui.Say("Generated mock Artifact with success :)")
 	return artifact, nil
 }
 
