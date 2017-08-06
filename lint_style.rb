6d7b234dcda0f6107774b9f3b1d6a2199c57f6f5
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-08-06 23:16:25 +0200

    lint: disable line length checking on code blocks

--- lint_style.rb
+++ lint_style.rb
@@ -1,3 +1,4 @@
 all
-rule 'MD026', :punctuation => ".,;:"  # Default is ".,;:!?"
 rule 'MD007', :indent => 4
+rule 'MD013', :code_blocks => false
+rule 'MD026', :punctuation => ".,;:"  # Default is ".,;:!?"
