6e2acb40cfeb5ef24612880fc75058a8a0710c0e
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-08-27 00:31:45 +0200

    ui_mock: fix lint by adding doc comments

--- packer-lib-mock/ui_mock.go
+++ packer-lib-mock/ui_mock.go
@@ -5,9 +5,9 @@ import (
 	"log"
 )
 
+// MockUI is a mock implementation of Packer's Ui interface (packer/ui.go)
+// It only includes counter to each of its methods, which will not do anything
 type MockUI struct {
-	// Counters for each method call on this struct
-
 	SayCount     int
 	AskCount     int
 	MessageCount int
@@ -15,27 +15,32 @@ type MockUI struct {
 	MachineCount int
 }
 
+// Say counts calls to itself. Mock of the Packer Ui.Say() method
 func (ui *MockUI) Say(message string) {
 	log.Printf("ui.Say mock call")
 	ui.SayCount++
 }
 
+// Ask counts calls to itself. Mock of the Packer Ui.Ask() method
 func (ui *MockUI) Ask(query string) (string, error) {
 	log.Printf("ui.Ask mock call")
 	ui.AskCount++
 	return fmt.Sprintf("I am a mocked Response. Your query: %s", query), nil
 }
 
+// Message counts calls to itself. Mock of the Packer Ui.Message() method
 func (ui *MockUI) Message(message string) {
 	log.Printf("ui.Message mock call")
 	ui.MessageCount++
 }
 
+// Error counts calls to itself. Mock of the Packer Ui.Error() method
 func (ui *MockUI) Error(message string) {
 	log.Printf("ui.Error mock call")
 	ui.ErrorCount++
 }
 
+// Machine counts calls to itself. Mock of the Packer Ui.Machine() method
 func (ui *MockUI) Machine(t string, args ...string) {
 	log.Printf("ui.Machine mock call")
 	ui.MachineCount++
